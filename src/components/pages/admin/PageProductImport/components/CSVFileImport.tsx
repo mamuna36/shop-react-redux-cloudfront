74280dcd218ef88a2c7935ba265aa27f3cb35d50
Mamuna Anwar <Mamuna_Anwar@epam.com>
2023-03-16 16:47:02 +0100

    solution to task-5-FE integration

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import axios from "axios";
 
 type CSVFileImportProps = {
   url: string;
@@ -24,23 +25,24 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
 
   const uploadFile = async () => {
     console.log("uploadFile to", url);
+    console.log("File to upload: ", file?.name);
 
     // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
+    const response = await axios({
+      method: "GET",
+      url,
+      params: {
+        name: encodeURIComponent(file ? file.name : "file"),
+      },
+    });
+
+    console.log("Uploading to: ", response);
     // const result = await fetch(response.data, {
     //   method: "PUT",
     //   body: file,
     // });
     // console.log("Result: ", result);
-    // setFile("");
+    //setFile(undefined);
   };
   return (
     <Box>
