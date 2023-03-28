fb6a3618b2749dbb7f318f963b951cc22e0a7a01
Mamuna Anwar <Mamuna_Anwar@epam.com>
2023-03-28 22:45:05 +0200

    fixed some tests

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://57p9vrm503.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://6dccs7m0sl.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://aw6v1rl990.execute-api.us-east-1.amazonaws.com/dev/products",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
