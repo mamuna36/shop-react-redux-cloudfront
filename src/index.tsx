d951b75b86b484051073ce4c6b520d3ce8c81142
Mamuna Anwar <Mamuna_Anwar@epam.com>
2023-03-28 12:01:57 +0200

    fixed some issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from '~/components/App/App'
-import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material/styles'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { theme } from '~/theme'
-import axios from 'axios'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "~/components/App/App";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { theme } from "~/theme";
+import axios from "axios";
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: { refetchOnWindowFocus: false, retry: false, staleTime: Infinity }
-  }
-})
+    queries: { refetchOnWindowFocus: false, retry: false, staleTime: Infinity },
+  },
+});
 axios.interceptors.response.use(undefined, (error) => {
   if ([401, 403].includes(error.response?.status)) {
     alert(
       `Error message: ${
-        error.response.data?.message || 'Unknown error happened'
+        error.response.data?.message || "seems like an authorization issue"
       }; status code: ${error.response.status}`
-    )
+    );
   }
 
-  return Promise.reject(error)
-})
+  return Promise.reject(error);
+});
 
 if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser')
-  worker.start({ onUnhandledRequest: 'bypass' })
+  const { worker } = await import("./mocks/browser");
+  worker.start({ onUnhandledRequest: "bypass" });
 }
 
-const container = document.getElementById('app')
+const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!)
+const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -46,4 +46,4 @@ root.render(
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
